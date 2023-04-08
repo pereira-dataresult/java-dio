401d5bd9bbfcfc875e31278756c17ab20bf4707b
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-08 20:46:12 -0300

    adicionando os operadores

---
+++ dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores {
+
+    public static void main(String[] args) throws Exception {
+
+        String concatenacao = "?";
+            System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+            System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+            System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+            System.out.println(concatenacao);
+
+
+    }
+}
