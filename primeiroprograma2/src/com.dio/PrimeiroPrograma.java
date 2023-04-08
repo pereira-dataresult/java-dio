a9e0b5d96d53cf78922ee32037269f9ca86a2cd1
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-08 15:02:19 -0300

    subir novos codigos

---
+++ primeiroprograma2/src/com.dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    
+    Gato gato = new Gato();
+    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("Hello World " + (a+b));
+    }
+}
