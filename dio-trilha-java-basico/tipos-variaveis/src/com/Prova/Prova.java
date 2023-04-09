5153d595dc41c35c9e1b1b91b801c411036fa80c
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-09 16:11:08 -0300

    subindo prova

---
+++ dio-trilha-java-basico/tipos-variaveis/src/com/Prova/Prova.java
@@ -0,0 +1,12 @@
+package com.Prova;
+
+public class Prova {
+
+    public static void main(String[] args) throws Exception {
+
+        int numero1 = 1;
+        String numero2 = "2";
+
+        System.out.println(numero1 + numero2);
+    }
+}
